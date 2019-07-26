a17b8581b2eae4c65ba9986a591a220e3aec8853
Deepa.Singh <deepa11031998@gmail.com>
2019-07-26 17:12:43 +0530

    new added

--- greeting.js
+++ greeting.js
@@ -1,6 +1,11 @@
 (function(){
     function greet(){
-    console.log("Hii,help us to suggest you the required monitoring devices");
+    const random = require('random');
+    greeting=['Hii,help us to suggest you the required monitoring devices',"Welcome! Find the Monitoring Devices you need ",
+    "Hello, We are here to suggest you monitoring devices "];
+
+    console.log("\n-----------------------");
+    console.log(greeting[random.int(min = 0, max = 2)]);
     }
     module.exports={greetFun:greet};
 })();
