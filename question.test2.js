549f3f462b77cbc8f55402ff049532697ffb4328
Deepa-3010 <53004989+Deepa-3010@users.noreply.github.com>
2019-07-30 15:52:55 +0530

    Add files via upload

---
+++ question.test2.js
@@ -0,0 +1,21 @@
+const question = require('../question')
+const questionHelper = require('../questionHelper')
+const sinon = require('sinon')
+let questionHelperStub;
+describe('Test question flow 2', function() {
+    before(function() {
+      questionHelperStub = sinon.stub(questionHelper,'askQuestion')
+    })
+    it('select based on touch devices', async function() {
+      questionHelperStub.resolves(1)
+      await question.questioning();
+      it("slect touch devices",async function(){
+        questionHelperStub.resolves(1);
+        await question.touchSelected();
+        it('display only on touch',async function(){
+          questionHelperStub.resolves(1);
+          await question.touch();
+        })
+      })
+    });
+  });
