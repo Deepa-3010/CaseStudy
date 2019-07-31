6854480a75d2cf345443676c7e57156842a73985
Deepa-3010 <53004989+Deepa-3010@users.noreply.github.com>
2019-07-31 10:46:52 +0530

    Add files via upload

--- testClass.js
+++ testClass.js
@@ -7,35 +7,32 @@ let questionExport=require("../question")
 const questionHelper = require('../questionHelper');
 let displayAgentExport=require("../displayDevices");
 const monitoringDeviceExport=require('../monitoringDevice')
-describe('to check it controller is executed',function(){
-    it('to check',function(){
-        controllerExport.controller();
-     } )
-     it('check if main called',function(){
-         mainExport.mainObject(global);
-     })
-     it('check if greeting is called',function(){
-          greetingExport.greetFun();
-     })
-     it('check if questioing is called',function(){
-        questionExport.questioning();
-   })
-it('check if displayAgent is called for both touch and size',function(){
+describe('to check each function call',function(){
+    it('check if main called',function(){
+        mainExport.mainObject(global);
+    })
+    it('check if greeting is called',function(){
+        greetingExport.greetFun();
+    })
+    it('check if questioing is called',function(){
+    questionExport.questioning();
+    })
+    it('check if displayAgent is called for both touch and size',function(){
     displayAgentExport.display(global,true,10);
-})
-it('check if monitoring is called for both touch and size',function(){
+    })
+    it('check if monitoring is called for both touch and size',function(){
     monitoringDeviceExport.result(global,true,10);
-})
-it('check if displayAgent is called for tuch',function(){
+    })
+    it('check if displayAgent is called for touch',function(){
     displayAgentExport.displayTouch(global,true);
-})
-it('check if displayAgent is called for size',function(){
+    })
+    it('check if displayAgent is called for size',function(){
     displayAgentExport.displaySize(global,10);
-})
-it('check if monitoring is called for touch',function(){
+    })
+    it('check if monitoring is called for touch',function(){
     monitoringDeviceExport.resultTouch(global,true);
-})
-it('check if monitoring is called for size',function(){
+    })
+    it('check if monitoring is called for size',function(){
     monitoringDeviceExport.resultSize(global,10);
-})
+    })
 })
