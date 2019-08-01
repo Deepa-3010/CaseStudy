b480bb4faea255fb527799bd55464e5eef66c200
Deepa-3010 <53004989+Deepa-3010@users.noreply.github.com>
2019-08-01 16:51:55 +0530

    Add files via upload

--- monitoringDevice.js
+++ monitoringDevice.js
@@ -78,17 +78,16 @@ async function resultOnlyBySize(size)
 }
 async function nextCall()
 {
-    let questionExport=require('./question');
     let questionHelperExport=require('./questionHelper');
-    var answer=await questionHelperExport.askQuestion("\n Press 1:for new search\n Press 2:to exit\n");
+    let questionExport=require('./question');
+    var answer=await questionHelperExport.askQuestion("\n Press 1:to continue\n Press 2:to exit\n");
     if(parseInt(answer)==1)
     {
         questionExport.questioning();
-
     }
     else
     {
-       
+       //let moduleExport=require('./modules');
        questionExport.exitMessage();
     }
 }
--- question.js
+++ question.js
@@ -1,6 +1,6 @@
 const questionHelper = require('./questionHelper');
 
-var questionArray = ["Press 1:Display devices based on Touch\nPress 2:Display devices by Screen Size\n", "Press 1:Touch screen\nPress 2:Non-Touch screen \nPress 3:To go to the previous menu\n", "Enter the size (9,10,12,14,15) in inches\n",
+var questionArray = ["Press 1:Display devices based on Touch\nPress 2:Display devices by Screen Size\n", "Press 1:Touch screen\nPress 2:Non-Touch screen \nPress 3:To go to the previous menu\n", "Enter the size\n",
  "Press 1:Display devices only by touch\nPress 2:Filter by Screen Size\nPress 3:To go to the previous menu\n",
  "Press 1:Display devices only by screen size\nPress 2:Filter by touch\nPress 3:To go to the previous menu\n"];
 
@@ -8,6 +8,7 @@ var questionArray = ["Press 1:Display devices based on Touch\nPress 2:Display de
 var _touch, _size;
 var touchIfSelected = false;
 
+//let checkBeginFunctionExport=require("./modules")
 let displayAgentExport = require("./displayDevices");
 var questionIndex=0;
 async function beginQuestion() {
@@ -28,10 +29,10 @@ async function beginQuestion() {
     }
 }
 async function touchSelected(questionIndex) {
-    
+    touchIfSelected = true;
     var answer2 = await questionHelper.askQuestion(questionArray[1]);
     if (parseInt(answer2) == 0) {
-        exitMessage();
+      exitMessage();
 
     }
     else if (parseInt(answer2)==3){
@@ -42,7 +43,6 @@ async function touchSelected(questionIndex) {
     else {
          questionIndex=1;
         console.log("\n-----------------------")
-        touchIfSelected = true;
         _touch = touch(answer2);
 
         var answer3 = await questionHelper.askQuestion(questionArray[3]);
@@ -57,15 +57,10 @@ async function touchSelected(questionIndex) {
         else {
             console.log("\n-----------------------")
             if (parseInt(answer3) == 1) {
-                displayAgentExport.displayTouch(global, _touch);
+                displayAgentExport.displayTouch(global.Philips.HealthCare.MonitoringDevices, _touch);
             }
             if (parseInt(answer3) == 2) {
                 var answer4 = await questionHelper.askQuestion(questionArray[2]);
-                while(checkIfSizeExists(answer4)==0)
-                {
-                    console.log("\nSize not available.Please enter a valid size(9,10,12,14,15)");
-                    answer4=await questionHelper.askQuestion(questionArray[2]) ;  
-                }
                 if (parseInt(answer4) == 0) {
                     exitMessage();
 
@@ -73,7 +68,7 @@ async function touchSelected(questionIndex) {
                 else {
                     console.log("\n-----------------------")
                     _size = answer4;
-                    displayAgentExport.display(global, _touch, _size);
+                    displayAgentExport.display(global.Philips.HealthCare.MonitoringDevices, _touch, _size);
                 }
             }
         }
@@ -83,11 +78,6 @@ async function touchSelected(questionIndex) {
 async function sizeSelected(questionIndex) {
 
     var answer2 = await questionHelper.askQuestion(questionArray[2]);
-    while(checkIfSizeExists(answer2)==0)
-    {
-            console.log("\nSize not available.Please enter a valid size(9,10,12,14,15)");
-            answer2=await questionHelper.askQuestion(questionArray[2]) ;  
-    }
     if (parseInt(answer2) == 0) {
         exitMessage();
 
@@ -95,10 +85,9 @@ async function sizeSelected(questionIndex) {
     else {
         console.log("\n-----------------------")
         _size = answer2;
-        
 
         if (touchIfSelected == true) {
-            displayAgentExport.display(global, _touch, _size);
+            displayAgentExport.display(global.Philips.HealthCare.MonitoringDevices, _touch, _size);
         }
         else {
             questionIndex=2;
@@ -110,7 +99,7 @@ async function sizeSelected(questionIndex) {
             else {
                 console.log("\n-----------------------")
                 if (answer3 == 1) {
-                    displayAgentExport.displaySize(global, _size);
+                    displayAgentExport.displaySize(global.Philips.HealthCare.MonitoringDevices, _size);
                 }
                 else if(parseInt(answer3)==3)
                 {
@@ -131,7 +120,7 @@ async function sizeSelected(questionIndex) {
                     else {
                         console.log("\n-----------------------")
                         _touch = touch(answer4);
-                        displayAgentExport.display(global, _touch, _size);
+                        displayAgentExport.display(global.Philips.HealthCare.MonitoringDevices, _touch, _size);
                     }
                 }
             }
@@ -141,7 +130,8 @@ async function sizeSelected(questionIndex) {
 
 function touch(answer) {
     if (answer == 0) {
-        exitMessage();
+        console.log("Thank You");
+        process.exit();
     }
     if (answer == 1)
         return true;
@@ -153,24 +143,11 @@ function touch(answer) {
     }
 
 }
-
 async function exitMessage()
 {
     console.log("Thank You !");
     process.exit();
 }
-
-function checkIfSizeExists(size)
-{
-    if(size==9 || size==10 || size==12 || size==14 || size==15 )
-    {
-        return 1;
-    }
-    else{
-        return 0;
-    }
-}
-
 module.exports = { questioning: beginQuestion,exitMessage:exitMessage};
 
 
