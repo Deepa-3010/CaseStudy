f8f7a76d139ad5d4a34c1f91df89d736182babfc
Deepa-3010 <53004989+Deepa-3010@users.noreply.github.com>
2019-08-21 08:18:14 +0530

    Add files via upload

--- question.js
+++ question.js
@@ -1,143 +1,155 @@
 const questionHelper = require('./questionHelper');
-var questionArray=["Press 1:Display devices based on Touch\nPress 2:Display devices by Screen Size\n","Press 1:Touch screen\nPress 2:Non-Touch screen \n","Enter the size\n",
-"Press 1:Display devices only by touch\nPress 2:Filter by Screen Size\n",
-"Press 1:Display devices only by screen size\nPress 2:Filter by touch\n"];
 
-var _touch,_size;
-var touchIfSelected=false;
+var questionArray = ["Press 1:Display devices based on Touch\nPress 2:Display devices by Screen Size\n", "Press 1:Touch screen\nPress 2:Non-Touch screen \nPress 3:To go to the previous menu\n", "Enter the size\n",
+ "Press 1:Display devices only by touch\nPress 2:Filter by Screen Size\nPress 3:To go to the previous menu\n",
+ "Press 1:Display devices only by screen size\nPress 2:Filter by touch\nPress 3:To go to the previous menu\n"];
 
-let displayAgentExport=require("./displayDevices");
 
-async function beginQuestion(){
+var _touch, _size;
+var touchIfSelected = false;
+
+//let checkBeginFunctionExport=require("./modules")
+let displayAgentExport = require("./displayDevices");
+var questionIndex=0;
+async function beginQuestion() {
 
     console.log("\n-----------------------")
-    var answer1=await questionHelper.askQuestion(questionArray[0]);
+    var answer1 = await questionHelper.askQuestion(questionArray[questionIndex]);
     console.log("\n-----------------------")
-    if(parseInt(answer1)==0)
-    {
-        console.log("Thank you");
-        process.exit();
+    if (parseInt(answer1) == 0) {
+        exitMessage();
     }
-    if(parseInt(answer1)==1)
-        touchSelected();
-    if(parseInt(answer1)==2)
-    sizeSelected();
-    if(answer1!=1 && answer1!=2)
-    {
+    if (parseInt(answer1) == 1)
+        touchSelected(0);
+    if (parseInt(answer1) == 2)
+        sizeSelected(0);
+    if (answer1 != 1 && answer1 != 2) {
         console.log("You have entered wrong input");
         beginQuestion();
     }
 }
-async function touchSelected(){
-    touchIfSelected=true;
-    var answer2=await questionHelper.askQuestion(questionArray[1]);
-    if(parseInt(answer2)==0)
-    {
-        console.log("Thank you");
-        process.exit();
-    }
-    else{
-    console.log("\n-----------------------")
-   _touch=touch(answer2);
+async function touchSelected(questionIndex) {
+    touchIfSelected = true;
+    var answer2 = await questionHelper.askQuestion(questionArray[1]);
+    if (parseInt(answer2) == 0) {
+      exitMessage();
 
-   // console.log(`Touch ${_touch}`)
+    }
+    else if (parseInt(answer2)==3){
 
-    var answer3=await questionHelper.askQuestion(questionArray[3]);
-    if(parseInt(answer3)==0)
-    {
-        console.log("Thank you");
-        process.exit();
+        beginQuestion(questionIndex);
+
     }
-    else{
-    console.log("\n-----------------------")
-    if(parseInt(answer3)==1)
-    {
-        displayAgentExport.displayTouch(global,_touch);
-    }
-    if(parseInt(answer3)==2)
-    {
-        var answer4=await questionHelper.askQuestion(questionArray[2]);
-        if(parseInt(answer4)==0)
-        {
-            console.log("Thank you");
-              process.exit();
-        }
-        else{
+    else {
+         questionIndex=1;
         console.log("\n-----------------------")
-        _size=answer4;
-        displayAgentExport.display(global,_touch,_size);
-        }
-    } 
-} 
-}  
-}
+        _touch = touch(answer2);
 
-async function sizeSelected(){
-    
-    var answer2=await questionHelper.askQuestion(questionArray[2]);
-    if(parseInt(answer2)==0)
-    {
-        console.log("Thank you");
-              process.exit();
-    }
-    else{
-    console.log("\n-----------------------")
-    _size=answer2;
+        var answer3 = await questionHelper.askQuestion(questionArray[3]);
+        if (parseInt(answer3) == 0) {
+            exitMessage();
 
-    if(touchIfSelected==true){
-        displayAgentExport.display(global,_touch,_size);
-    }
-    else{
-        var answer3=await questionHelper.askQuestion(questionArray[4]);
-        if(parseInt(answer3)==0)
-        {
-            console.log("Thank you");
-              process.exit();
         }
-        else
+        else if (parseInt(answer3)==3)
         {
-        console.log("\n-----------------------")
-        if(answer3==1){
-            displayAgentExport.displaySize(global,_size);
+            touchSelected(questionIndex);
         }
-        else if(answer3==2){
-            var answer4=await questionHelper.askQuestion(questionArray[1]);
-            if(parseInt(answer4)==0)
-            {
-                console.log("Thank you");
-              process.exit();
-            }
-            else{
+        else {
             console.log("\n-----------------------")
-            _touch=touch(answer4);
-            displayAgentExport.display(global,_touch,_size);
+            if (parseInt(answer3) == 1) {
+                displayAgentExport.displayTouch(global.Philips.HealthCare.MonitoringDevices, _touch);
+            }
+            if (parseInt(answer3) == 2) {
+                var answer4 = await questionHelper.askQuestion(questionArray[2]);
+                if (parseInt(answer4) == 0) {
+                    exitMessage();
+
+                }
+                else {
+                    console.log("\n-----------------------")
+                    _size = answer4;
+                    displayAgentExport.display(global.Philips.HealthCare.MonitoringDevices, _touch, _size);
+                }
             }
         }
     }
-    }
-}
 }
 
-function touch(answer)
-{
-    if(answer==0)
-    {
+async function sizeSelected(questionIndex) {
+
+    var answer2 = await questionHelper.askQuestion(questionArray[2]);
+    if (parseInt(answer2) == 0) {
+        exitMessage();
+
+    }
+    else {
+        console.log("\n-----------------------")
+        _size = answer2;
+
+        if (touchIfSelected == true) {
+            displayAgentExport.display(global.Philips.HealthCare.MonitoringDevices, _touch, _size);
+        }
+        else {
+            questionIndex=2;
+            var answer3 = await questionHelper.askQuestion(questionArray[4]);
+            if (parseInt(answer3) == 0) {
+                exitMessage();
+
+            }
+            else {
+                console.log("\n-----------------------")
+                if (answer3 == 1) {
+                    displayAgentExport.displaySize(global.Philips.HealthCare.MonitoringDevices, _size);
+                }
+                else if(parseInt(answer3)==3)
+                {
+                    sizeSelected(questionIndex);
+                }
+
+                else if (answer3 == 2) {
+                    var answer4 = await questionHelper.askQuestion(questionArray[1]);
+                    if (parseInt(answer4) == 0) {
+                        exitMessage();
+
+                    }
+                    else if(parseInt(answer4)==3)
+                    {
+
+                        sizeSelected(questionIndex);
+                    }
+                    else {
+                        console.log("\n-----------------------")
+                        _touch = touch(answer4);
+                        displayAgentExport.display(global.Philips.HealthCare.MonitoringDevices, _touch, _size);
+                    }
+                }
+            }
+        }
+    }
+}
+
+function touch(answer) {
+    if (answer == 0) {
         console.log("Thank You");
         process.exit();
     }
-    if(answer==1)
-     return true;
-    if(answer==2) 
-    return false;  
-    if(answer!=1 && answer!=2)
-    {
+    if (answer == 1)
+        return true;
+    if (answer == 2)
+        return false;
+    if (answer != 1 && answer != 2) {
         console.log("You have entered a wrong input try again from the start");
         beginQuestion();
     }
 
 }
-module.exports={questioning:beginQuestion,touchSelected:touchSelected,touch:touch};
+async function exitMessage()
+{
+    console.log("Thank You !");
+    process.exit();
+}
+module.exports = { questioning: beginQuestion,exitMessage:exitMessage};
 
 
 
-//module.exports={questioning:beginQuestion};
+
